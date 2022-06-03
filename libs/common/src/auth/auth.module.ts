5a5fafa1bccca7dfa70d5eee80ac41525ee55d49
sam <51648328+SamDTech@users.noreply.github.com>
2022-06-03 11:34:04 +0100

    add the authentication library

---
+++ libs/common/src/auth/auth.module.ts
@@ -0,0 +1,18 @@
+import { AUTH_SERVICE } from './services';
+import { RmqModule } from '@app/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import cookieParser from 'cookie-parser';
+
+@Module({
+  imports: [
+    RmqModule.register({
+      name: AUTH_SERVICE,
+    }),
+  ],
+  exports: [RmqModule],
+})
+export class AuthModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(cookieParser()).forRoutes('*');
+  }
+}
